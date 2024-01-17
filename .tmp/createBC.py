1be52c70edda18e44e36116fbffd37ad7c17e477
yue523 <52612605+yue523@users.noreply.github.com>
2024-01-17 16:56:35 +0900

    update 01/171 16:56

--- .tmp/createBC.py
+++ .tmp/createBC.py
@@ -1,103 +1,75 @@
 '''
-作成されたトランザクションからブロックを生成するプログラム
-※ブロックがdataファイル内に8個ある必要がある.
+新しくブロックチェーンを更新するプログラム
 '''
-
-from datetime import datetime 
-import json
-import hashlib
-import uuid
 import os
+import json
 import shutil
-import glob
-    
-class Block:
 
-    def calculate_merkle(self):
-        # ディレクトリ内のJSONファイルのパスを取得
-        directory_path = "../data/transaction"
-        json_files = [f for f in os.listdir(directory_path) if f.endswith('.json')]
+# ブロックプール内から一番小さいタイムスタンプのブロックを取得
+def getNewBL(Blockfolder):
+    # フォルダ内のJSONファイルの一覧を取得
+    json_files = [f for f in os.listdir(Blockfolder) if f.endswith('.json')]
 
-        # JSONファイルのパスとtimestampの値を格納するリスト
-        files_with_timestamp = []
-        # ディレクトリ内のJSONファイルからtimestampの値を取得して小さい順にソート
-        for json_file in json_files:
-            file_path = os.path.join(directory_path, json_file)
-            with open(file_path, 'r') as f:
-                data = json.load(f)
-                timestamp_value = data.get('timestamp')
-                files_with_timestamp.append((file_path, timestamp_value))
-        sorted_files = sorted(files_with_timestamp, key=lambda x: x[1])
+    min_timestamp = float('inf')  # 初期値を無限大に設定
+    min_timestamp_file_path = None
 
-        # 最初の8つのファイルの中身を文字列型の配列に格納
-        file_contents = []
-        for file_path, _ in sorted_files[:8]:
-            with open(file_path, 'r') as f:
-                content = f.read()
-                file_contents.append(content)
+    for json_file in json_files:
+        file_path = os.path.join(Blockfolder, json_file)
 
-        # マークルルートを計算
-        hashed_values = [hashlib.sha256(data.encode('utf-8')).hexdigest() for data in file_contents]
-        while len(hashed_values) > 1:
-            temp = []
-            for i in range(0, len(hashed_values), 2):
-                # 2つのハッシュを連結してハッシュ化
-                combined_hash = hashlib.sha256((hashed_values[i] + hashed_values[i + 1]).encode('utf-8')).hexdigest()
-                temp.append(combined_hash)
-            # 新しいハッシュリストを使用してループを続行
-            hashed_values = temp
-        merkle_root = hashed_values[0]
+        # JSONファイルを読み込んでtimestampを取得
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+            timestamp = data.get('timestamp', float('inf'))
+            if timestamp < min_timestamp:
+                min_timestamp = timestamp
+                min_timestamp_file_path = file_path
 
-        # 移動先のディレクトリ
-        destination_directory = "../data/.transaction"
+    return min_timestamp_file_path
 
-        # 最初の8つのファイルを移動
-        for file_path, _ in sorted_files[:8]:
-            # ファイルを移動
-            shutil.move(file_path, os.path.join(destination_directory, os.path.basename(file_path)))
+# ブロックチェーンにブロックを追加する関数
+def addtoBC(BCjson,BLFpath):
+    # タイムスタンプが小さいブロックを取得
+    BLpath = getNewBL(BLFpath)
+    # 新しいブロックのindexの作成
+    with open(BCpath, 'r') as file:
+        BCjson = json.load(file)
+    lastID = BCjson[-1]['index']
+    nextID = lastID + 1
+    # 新しいブロックのjsonを作成
+    with open(BLpath, "r", encoding="utf-8") as file:
+        newBC_json = json.load(file)
+    # 追加するブロックの作成と更新
+    newBL = {
+        'index': nextID,
+        'block': newBC_json
+    }
+    BCjson.append(newBL)
 
-        return merkle_root
+    # JSONファイルの読み込み
+    with open(BCpath, 'r') as file:
+        # ファイルを書き込みモードで開いてJSONデータを書き込む
+        with open(BCpath, 'w') as write_file:
+            json.dump(BCjson, write_file, indent=2)
 
-    def createBL(self): 
-        # マークルルートの作成
-        merkle = self.calculate_merkle()
-
-        # タイムスタンプの作成
-        now = datetime.now()
-        timestamp = int(now.timestamp())
-
-        # ブロックチェーン最後のハッシュの取得
-        prevHash = ""
-        # ノンス値の作成
-        # nonce = random.randomint(1, 1000000)
-        nonce = "none"
-        # ブロックの作成
-        newBL = {
-            "prevHash": prevHash,
-            "hash": merkle,
-            "timestamp": timestamp,
-            "nonce": nonce
-        }
-        # JSONファイルへの書き込み
-        block_path = '../data/block/' + str(uuid.uuid4()) + '.json'
-        with open(block_path, 'w') as json_file:
-            json.dump(newBL, json_file, indent=2)
+    # 追加したブロックを別のフォルダに移動する
+    BLmoveto = "./data/.block"
+    shutil.move(BLpath, BLmoveto)
 
 if __name__ == "__main__":
+    # ブロックチェーンの読み込み
+    BCpath = './data/blockchain/blockchain.json'
+    with open(BCpath, 'r') as json_file:
+        BCjson = json.load(json_file)
+    
+    # フォルダ内のファイルを取得
+    BLFpath = './data/block'
 
-    ################
-    # ブロックの処理
-    ################
-    while True:
-        # 指定されたディレクトリ内のJSONファイルを検索
-        TXfiles = glob.glob(os.path.join("../data/transaction", '*.json'))
-        TXcount = len(TXfiles)
+    # ブロックへの追加
+    while True :
+        # フォルダ内にファイルが存在するかチェック
+        files_in_folder = os.listdir(BLFpath)
 
-        # dataファイル内に8つ以上のファイルがあった場合、
-        # blockクラスを呼び出してcreateBLを行う.
-        if TXcount >= 8:
-            newBL = Block()
-            newBL.createBL()
-        # main.pyはelse分はいらない
-        else :
-            break
+        # ファイルが存在する場合、addtoBC関数を実行
+        if files_in_folder:
+            addtoBC(BCjson, BLFpath)
+
