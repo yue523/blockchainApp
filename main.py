629a77314f0d4f836f0fcd9396de2265405e2ef5
yue523 <52612605+yue523@users.noreply.github.com>
2024-01-17 15:51:24 +0900

    update 01/17 15:51

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import hashlib
 import json
 import socket
+import shutil
 import random
 import uuid
 import os
@@ -120,25 +121,55 @@ class Block:
 # ブロックチェーンクラス
 ####################
 class Blockchain:
-    def __init__(self, BC):
-        self.BC = BC
+    # ブロックプール内から一番小さいタイムスタンプのブロックを取得
+    def getNewBL(self, Blockfolder):
+        # フォルダ内のJSONファイルの一覧を取得
+        json_files = [f for f in os.listdir(Blockfolder) if f.endswith('.json')]
 
-    # 新しいブロックチェーンを作成する関数
-    def createBC(self):
-        with open("blockchain.json", "w") as file:
-            file.write("")
-    
-    # 前のブロックチェーンのハッシュ値を取得
-    def getPreBC(self):
-        with open("blockchain.json", 'r') as file:
-            prevHash = json.load(file)
-        return prevHash
-    
-    # 新しいブロックチェーンを作成する関数
-    def create_newBC(self):
-        newBC=self.BC
-        with open("blockchain.json", "w") as file:
-            json.write(newBC)
+        min_timestamp = float('inf')  # 初期値を無限大に設定
+        min_timestamp_file_path = None
+
+        for json_file in json_files:
+            file_path = os.path.join(Blockfolder, json_file)
+
+            # JSONファイルを読み込んでtimestampを取得
+            with open(file_path, 'r') as f:
+                data = json.load(f)
+                timestamp = data.get('timestamp', float('inf'))
+                if timestamp < min_timestamp:
+                    min_timestamp = timestamp
+                    min_timestamp_file_path = file_path
+
+        return min_timestamp_file_path
+
+    # ブロックチェーンにブロックを追加する関数
+    def addtoBC(self, BCjson, BLFpath):
+        # タイムスタンプが小さいブロックを取得
+        BLpath = self.getNewBL(BLFpath)
+        # 新しいブロックのindexの作成
+        with open(BCpath, 'r') as file:
+            BCjson = json.load(file)
+        lastID = BCjson[-1]['index']
+        nextID = lastID + 1
+        # 新しいブロックのjsonを作成
+        with open(BLpath, "r", encoding="utf-8") as file:
+            newBC_json = json.load(file)
+        # 追加するブロックの作成と更新
+        newBL = {
+            'index': nextID,
+            'block': newBC_json
+        }
+        BCjson.append(newBL)
+
+        # JSONファイルの読み込み
+        with open(BCpath, 'r') as file:
+            # ファイルを書き込みモードで開いてJSONデータを書き込む
+            with open(BCpath, 'w') as write_file:
+                json.dump(BCjson, write_file, indent=2)
+
+        # 追加したブロックを別のフォルダに移動する
+        BLmoveto = "./data/.block"
+        shutil.move(BLpath, BLmoveto)
 
 ####################
 # main関数
@@ -162,9 +193,16 @@ if __name__ == "__main__":
     newTX = sampleTX.createTX()
     sock.sendto(newTX, (CLIENT, PORT))
 
-    ########
+    #####################
     # ブロックチェーンに関するプログラム
-    ########
+    #####################
+    # ブロックチェーンの読み込み
+    BCpath = './data/blockchain/blockchain.json'
+    with open(BCpath, 'r') as json_file:
+        BCjson = json.load(json_file)
+    
+    # フォルダ内のファイルを取得
+    BLFpath = './data/block'
 
     ######################
     # 常時実行プログラム
@@ -178,6 +216,16 @@ if __name__ == "__main__":
         if TXcount >= 8:
             newBL = Block()
             newBL.createBL()
+        
+        ################################
+        # ブロックチェーンにブロックを追加
+        ################################
+        # フォルダ内にファイルが存在するかチェック
+        BLfolder = os.listdir(BLFpath)
+        # ファイルが存在する場合、blockchainクラスのaddtoBC関数を実行
+        if BLfolder:
+            newBC = Blockchain()
+            newBC.addtoBC(BCjson, BLFpath)        
 
         ################################
         # データをTX、BL、BCに仕分けして保存する
