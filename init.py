acadad8ef02f7bd9e914ef158d46a1545ff99fd2
yue523 <18fr030@ms.dendai.ac.jp>
2024-01-18 13:04:58 +0900

    update 01/18 13:04

---
+++ init.py
@@ -0,0 +1,27 @@
+##################################
+# 本アプリの初期設定を行うプログラム
+# アプリを開始する前に本プログラムを実行すること
+##################################
+
+import json
+import socket
+
+# ユーザーからの入力を取得
+user_input = input("名前を英語で入力してください: ")
+
+# ホストのIPアドレスを取得
+host_ip = socket.gethostbyname(socket.gethostname())
+
+# info.jsonファイルにデータを書き込む
+data = {
+    "name": user_input, 
+    "HOST": host_ip,
+    "CLIENT": '192.168.3.255',
+    "PORT": 50000
+    }
+
+with open("info.json", "w") as json_file:
+    json.dump(data, json_file, indent = 2)
+
+print("info.jsonが作成されました。")
+print("初期設定が完了しました。")
