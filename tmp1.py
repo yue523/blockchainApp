6583b319b4911d4cfbd20b1319da76338d3aa11b
yue523 <18fr030@ms.dendai.ac.jp>
2024-01-19 10:26:29 +0900

    update 01/19 10:26

--- tmp1.py
+++ tmp1.py
@@ -1,22 +1,14 @@
-from pynput import keyboard
+import keyboard
 
-# キーが押されたときの処理
-def on_press(key):
-    try:
-        # キーが 'q' だった場合、プログラムを終了
-        if key.char == 'q':
-            print("キー 'q' が押されたため、プログラムを終了します。")
-            return False
-    except AttributeError:
-        # 特殊キーが押された場合は無視
-        pass
+def main():
+    print("プログラムを開始します。q キーを押すと終了します。")
 
-# キーボードの監視を開始
-with keyboard.Listener(on_press=on_press) as listener:
-    # ここに処理を書く
     while True:
-        # 何か処理を行う
-        pass
+        if keyboard.is_pressed('q'):
+            print("q キーが押されたのでプログラムを終了します。")
+            break
 
-    # プログラムが終了したら監視を停止
-    listener.join()
+        # ここに継続的な処理を追加する
+
+if __name__ == "__main__":
+    main()
