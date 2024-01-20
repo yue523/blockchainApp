254b4df70aa4ad5e849753077c1bd07c6e448660
yue523 <18fr030@ms.dendai.ac.jp>
2024-01-20 16:12:38 +0900

    update 01/20 16:12

--- .tmp/sender.py
+++ .tmp/sender.py
@@ -4,8 +4,8 @@ import json
 # ソケットの作成
 sender_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# 送信先のIPアドレスとポート
-receiver_address = ('192.168.3.105', 12345)
+# 送信先のブロードキャストIPアドレスとポート
+receiver_broadcast_address = ('192.168.3.255', 12345)
 
 # 送信するデータの読み込み
 with open('./test.json', 'r') as file:
@@ -16,7 +16,7 @@ sender_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 # データの送信
 encoded_data = json.dumps(data_to_send).encode('utf-8')
-sender_socket.sendto(encoded_data, receiver_address)
+sender_socket.sendto(encoded_data, receiver_broadcast_address)
 
 # ソケットのクローズ
 sender_socket.close()
